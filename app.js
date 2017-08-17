fdd89f9570f9d7126e5bc0490c0215b8e68c4581
moeabdol <mohd.a.saed@gmail.com>
2017-08-17 21:14:36 +0300

    Basic app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+app.set("port", process.env.PORT || 3000);
+
+app.get("/", (req, res) => {
+  res.send(req.headers["user-agent"]);
+});
+
+if (!module.parent) {
+  app.listen(app.get("port"), () => {
+    console.log("App started on port " + app.get("port"));
+  });
+}
+
+module.exports = app;
