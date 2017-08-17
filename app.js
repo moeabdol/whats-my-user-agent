dbbcaec8fad20fa083b8df18f044c2f1f48030ce
moeabdol <mohd.a.saed@gmail.com>
2017-08-17 22:13:40 +0300

    Add index view

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const path    = require("path");
+const hbs     = require("express-handlebars");
 
 const app = express();
 
 app.set("port", process.env.PORT || 3000);
 
+app.engine("hbs", hbs({
+  extname: "hbs"
+}));
+app.set("views", path.resolve(__dirname, "views"));
+app.set("view engine", "hbs");
+
 app.get("/", (req, res) => {
-  res.type("text");
-  res.send(req.headers["user-agent"]);
+  const userAgent = req.headers["user-agent"] || "none";
+  if (req.accepts("html")) {
+    res.render("index", { userAgent: userAgent });
+  } else {
+    res.type("text");
+    res.send(userAgent);
+  }
 });
 
 if (!module.parent) {
